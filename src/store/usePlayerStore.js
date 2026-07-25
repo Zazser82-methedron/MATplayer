51cc8395b23116d673eaefd3f8b772decd9a210b
Zazser82-methedron <zazser82@gmail.com>
2026-07-25 07:31:09 +0300

    feat: add Zustand player store with transient audio-band updates

---
+++ src/store/usePlayerStore.js
@@ -0,0 +1,17 @@
+import { create } from 'zustand'
+
+export const usePlayerStore = create((set) => ({
+  currentArtistId: null,
+  currentTrackId: null,
+  currentTime: 0,
+  isPlaying: false,
+  uxMode: 'focus', // 'focus' | 'ambient' | 'utility'
+  audioBands: { bass: 0, mid: 0, treble: 0, level: 0 },
+
+  setArtist: (artistId) => set({ currentArtistId: artistId }),
+  setTrack: (trackId) => set({ currentTrackId: trackId }),
+  setTime: (time) => set({ currentTime: time }),
+  setPlaying: (playing) => set({ isPlaying: playing }),
+  setUxMode: (mode) => set({ uxMode: mode }),
+  setAudioBands: (bands) => set({ audioBands: bands }),
+}))
